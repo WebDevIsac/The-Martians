5dc37aff0cf3a5e8c056ff5e08b9bdbb99495e1a
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2018-11-07 20:00:29 +0100

    Added elements from Isac-branch manually

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,43 @@ document.getElementById("burgerIcon").addEventListener("click", () => {
   document.getElementById("burgerMenu").classList.toggle("slide");
 
 });
+
+
+
+
+
+const container = document.querySelector('.container');
+const buttonsContainer = document.querySelector('.buttons');
+const buttons = document.querySelectorAll('.buttons > div');
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+const activeBtn = document.querySelector('.active');
+
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        let currentBtn = buttons[i];
+        let newIndex = i + 1;
+        let currentImg = 'imgSlide' + newIndex;
+
+        container.setAttribute('id', currentImg);
+        currentBtn.setAttribute('id', 'active');
+
+
+        if (btn1 !== currentBtn) {
+            btn1.removeAttribute('id', 'active');
+            if (btn2 !== currentBtn) {
+                btn2.removeAttribute('id', 'active');
+            } else {
+                btn3.removeAttribute('id', 'active');
+            }
+
+        } else if (btn2 !== currentBtn) {
+            btn2.removeAttribute('id', 'active');
+            if (btn3 !== currentBtn) {
+                btn3.removeAttribute('id', 'active');
+            }
+        }
+    });
+}
--- index.php
+++ index.php
@@ -13,39 +13,53 @@
   <title>The Martians</title>
 </head>
 
+<header>
+
+  <!-- Desktop Navigation  -->
+
+  <nav class="mainNav">
+
+    <div class="desktopNav">
+      <div class="about button"><a href="#">Om</a></div>
+      <div class="logoContainer"><a href="#top"><img class="logoImg" src="assets/images/saabLogo.png" alt=""></a></div>
+      <div class="email button"><a href="#">Email</a></div>
+      <div class="catalogue button"><a  href="#">Produkt katalog</a></div>
+    </div>
+
+    <!-- Burger menu icon -->
+
+    <div class="burgerIcon" id="burgerIcon">
+      <div id="barOne" class="barOne"></div>
+      <div id="barTwo" class="barTwo"></div>
+      <div id="barThree" class="barThree"></div>
+    </div>
+
+    <!-- Mobile menu  -->
+
+    <div class="burgerMenu" id="burgerMenu">
+      <div class="aboutBurger"><a href="#">Om</a></div>
+      <div class="emailBurger"><a href="#">Email</a></div>
+      <div class="catalogueBurger"><a href="#">Produkt katalog</a></div>
+    </div>
+
+  </nav>
+</header>
+
 <body>
 
-  <header>
 
-    <!-- Desktop Navigation  -->
 
-    <nav class="mainNav">
+  <div class="container" id="imgSlide1">
+    <div class="textBox">
+      <p>Voluptatibus velit atque quis facilis quo voluptatem vel debitis.</p>
+    </div>
+    <div class="buttons">
+      <div class="btn1" id="active"></div>
+      <div class="btn2"></div>
+      <div class="btn3"></div>
+    </div>
+  </div>
 
-      <div class="desktopNav">
-        <div class="about button"><a href="#">Om</a></div>
-        <div class="logoContainer"><a href="#top"><img class="logoImg" src="assets/images/saabLogo.png" alt=""></a></div>
-        <div class="email button"><a href="#">Email</a></div>
-        <div class="catalogue button"><a  href="#">Produkt katalog</a></div>
-      </div>
-
-      <!-- Burger menu icon -->
-
-      <div class="burgerIcon" id="burgerIcon">
-        <div id="barOne" class="barOne"></div>
-        <div id="barTwo" class="barTwo"></div>
-        <div id="barThree" class="barThree"></div>
-      </div>
-
-      <!-- Mobile menu  -->
-
-      <div class="burgerMenu" id="burgerMenu">
-        <div class="aboutBurger"><a href="#">Om</a></div>
-        <div class="emailBurger"><a href="#">Email</a></div>
-        <div class="catalogueBurger"><a href="#">Produkt katalog</a></div>
-      </div>
-
-    </nav>
-  </header>
 
   <script src="assets/script.js" charset="utf-8"></script>
 
