ef435c4c7720e9b2ac026e94afd8205be57a171c
Laureta Dzika <xhika_l@hotmail.com>
2018-11-20 13:16:58 +0100

    added another form with download link ad

---
+++ assets/download.php
@@ -0,0 +1,7 @@
+<div class="download-form">
+	<img src="/assets/images/winter-tires.jpg">
+	<div>
+		<a href="/assets/example.txt" download="Example"><button type="submit">Download</a></button>
+	</div>
+</div>
+
