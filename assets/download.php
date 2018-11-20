0d7b9b63da7a64d767a6339f672237d6331fdc7d
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2018-11-20 13:22:44 +0100

    Fixed the image link for download form

--- assets/download.php
+++ assets/download.php
@@ -1,7 +1,6 @@
 <div class="download-form">
-	<img src="/assets/images/winter-tires.jpg">
+	<img src="assets/images/winter-tires.jpg">
 	<div>
 		<a href="/assets/example.txt" download="Example"><button type="submit">Download</a></button>
 	</div>
 </div>
-
