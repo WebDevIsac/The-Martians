e2113b032a6c84d898ddaebc03e4e03525bc0701
Laureta Dzika <xhika_l@hotmail.com>
2018-11-21 15:36:58 +0100

    added more content to data.php
    more page translation

--- assets/data.php
+++ assets/data.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 $data = [
-	'sv' => ['title' => 'Vi lyfter glädjen till körning', 'content' => file_get_contents(__DIR__.'/swe-lorem.txt')],
-	'en' => ['title' => 'We bring fun into driving', 'content' => file_get_contents(__DIR__.'/eng-lorem.txt')],
+	'sv' => ['title' => 'Vi lyfter glädjen till körning', 'signup' => 'Registrera dig idag!', 'button' => 'Registrera', 'download' => 'Broschyr', 'content' => file_get_contents(__DIR__.'/swe-lorem.txt')],
+	'en' => ['title' => 'We bring fun into driving', 'signup' => 'Signup to be a part of the adventure
+', 'button' => 'Register', 'download' => 'Brochure', 'content' => file_get_contents(__DIR__.'/eng-lorem.txt')],
 ];
--- assets/form.php
+++ assets/form.php
@@ -1,12 +1,12 @@
 <form action="/" method="post">
 	<div class="form">
 		<div>
-			<h1>Signup to be a part of the adventure</h1>
+			<h1><?= $language['signup']; ?></h1>
 		</div>
 		<div>
 			<input type="name" name="name" placeholder="Name">
 			<input type="email" name="email" placeholder="Email">
-			<button>Register</button>
+			<button><?= $language['button']; ?></button>
 		</div>
 	</div>
 </div>
