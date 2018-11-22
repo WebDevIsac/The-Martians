0bf330318d8404992ef47ea361e96fd3c256e07f
Laureta Dzika <xhika_l@hotmail.com>
2018-11-22 10:34:02 +0100

    removed fontAwesome cdn, no longer needed

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ if(!isset($language)) {
 	<link rel="stylesheet" href="assets/stylesheet/form.css">
 	<link rel="stylesheet" href="assets/stylesheet/mediaquery.css">
 	<link rel="icon" href="assets/images/saabfavicon.png">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 	<title>The Martians</title>
 </head>
 
