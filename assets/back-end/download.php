cb5c9f98c245d9ff00c5cfffa8d9cd897872a0ef
Laureta Dzika <xhika_l@hotmail.com>
2018-11-28 21:27:25 +0100

    refactoring forms

--- assets/back-end/download.php
+++ assets/back-end/download.php
@@ -1,4 +1,4 @@
-<div class="download-form">
+<div class="form download-form">
 	<img src="assets/images/Interior_TerrngBil_Jimmy.jpg">
 	<h1>The future is waiting for you</h1>
 	<a href="assets/brochure/example.txt" download="Example"><button class="button"><?= $language['download-button']; ?></button></a>
