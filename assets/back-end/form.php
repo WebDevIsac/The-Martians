51529c03e9db06a1ec3d086a97cd3f0e4c19d9d5
Laureta Dzika <xhika_l@hotmail.com>
2018-11-28 21:35:22 +0100

    refactoring forms pt2

--- assets/back-end/form.php
+++ assets/back-end/form.php
@@ -2,10 +2,8 @@
 	<div class="form">
 			<p><?= $language['content']; ?></p>
 			<h1><?= $language['signup']; ?></h1>
-		<div>
 			<input type="name" name="name" placeholder="Name">
 			<input type="email" name="email" placeholder="Email">
 			<button><?= $language['register-button']; ?></button>
 		</div>
-	</div>
 </form>
