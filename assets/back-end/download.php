d9db150585402a5ee1394c65ef7eb2467f989b6d
WebDevIsac <isac.code@gmail.com>
2018-11-29 18:21:10 +0100

    Added brochure as downloadable file

--- assets/back-end/download.php
+++ assets/back-end/download.php
@@ -1,5 +1,5 @@
 <div class="form download-form">
 	<img src="assets/images/Interior_TerrngBil_Jimmy.jpg">
 	<h1>The future is waiting for you</h1>
-	<a href="assets/brochure/example.txt" download="Example"><button class="button"><?= $language['download-button']; ?></button></a>
+	<a href="assets/brochure/fsbroschyr_grupp6.pdf" download="brochure"><button class="button"><?= $language['download-button']; ?></button></a>
 </div>
