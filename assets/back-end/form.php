93f61a36c528b78d43e72b2422bd7f0d31715db8
WebDevIsac <isac.code@gmail.com>
2018-11-30 09:29:03 +0100

    Added 'required' for form input

--- assets/back-end/form.php
+++ assets/back-end/form.php
@@ -2,8 +2,8 @@
 	<div class="form">
 			<p><?= $language['content']; ?></p>
 			<h1><?= $language['signup']; ?></h1>
-			<input type="name" name="name" placeholder="Name">
-			<input type="email" name="email" placeholder="Email">
+			<input type="name" name="name" placeholder="Name" required>
+			<input type="email" name="email" placeholder="Email" required>
 			<button><?= $language['register-button']; ?></button>
 		</div>
 </form>
